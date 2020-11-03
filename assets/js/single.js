e0bb1aa5a02d077f1ec85440d85abf0c89c6c522
CoCo Chanel <veta583518@gmail.com>
2020-11-03 16:17:26 -0600

    create second html page for issues

---
+++ assets/js/single.js
@@ -0,0 +1,62 @@
+// global variables
+var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoIssues = function(repo) {
+
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+            // format response as json
+            response.json().then(function(data) {
+                // pass response data to the dom function
+                displayIssues(data);
+            });
+        } 
+        else {
+            // request false 
+            alert("There was a problem with your request!")
+        }
+    });
+};
+
+var displayIssues = function(issues) {
+    // verify if repo has open issues
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    for (var i = 0; i < issues.length; ++i) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to the container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+        
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append to the container div
+        issueEl.appendChild(typeEl); 
+
+        //append to container DOM
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+getRepoIssues("facebook/react");
